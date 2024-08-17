09cd6f2cb79dc285e125bebbf961dd99cf504241
ZaptoInc <38966434+ZaptoInc@users.noreply.github.com>
2024-08-17 16:49:48 +0200

    testing online command

---
+++ commands/wow/online.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = function (api) {
+  return {
+    data: new SlashCommandBuilder()
+      .setName(api.i18n.translate("en", "wow.cmd.online"))
+      .setDescription(api.i18n.translate("en", "wow.desc.online"))
+      .setNameLocalizations(api.i18n.getAllTranslations("wow.cmd.online"))
+      .setDescriptionLocalizations(
+        api.i18n.getAllTranslations("wow.desc.online")
+      ),
+    async execute(interaction) {
+      let counter = 0;
+      let onlinePlayers = 0;
+      api.database.query("USE characters");
+      connection.query(
+        "select name from characters where online = 1",
+        (error, results1, fields) => {
+          console.log(results1);
+        }
+      );
+      let embed = new EmbedBuilder();
+      embed.setDescription("testing");
+      await interaction.reply({ embeds: [embed] });
+    },
+  };
+};
